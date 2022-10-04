5a45b824650e298a9d6874ea237753aaf4824bf1
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-03 20:06:56 -0700

    java code

---
+++ demo.js
@@ -0,0 +1,7 @@
+/*this is my first program
+now i knows the two ways to run program*/
+console.log("hellow");
+console.log("My name is Anuradha")
+console.info("This is Info")//info means infoemation
+console.warn("I am not that bad guy") //warn means warning
+console.error("oops failed to execute")
