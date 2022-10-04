b64d25e5cf48c9b0ea204dc900d7efbbb3cceaaa
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-04 06:54:25 -0700

    First Program Execution in js

---
+++ firstProgram.js
@@ -0,0 +1,6 @@
+console.log("My name is Anuradha Aldar")
+console.log( "My hobbies:- 1.Reading  2.Cooking 3.travlling")
+console.info("Strength:- 1.Hard Worker  2.Qucik learner 3.")
+console.warn("Weakness: 1.Emotional 2.Sensitive 3.Over Thinker")
+console.log(30+50)
+console.log(49*479)
