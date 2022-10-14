52cbe891561af9ad2037b65103fa17edf2219fc7
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-14 01:41:23 -0700

    assign

--- 06_assignment_using_for_loop.js
+++ 06_assignment_using_for_loop.js
@@ -1,4 +1,5 @@
-console.warn("===Q.1 Print numbers from 5 to 15 by incrementing 1======");
+console.log("");
+console.warn("Q.1 Print numbers from 5 to 15 by incrementing 1");
 console.log("");
 var result = '';
 for (let index = 5; index <=15; index++) {
@@ -8,7 +9,8 @@ for (let index = 5; index <=15; index++) {
 console.log(`Numbers 5 to 15:- ${result}`);
 console.log("");
 
-console.warn("===Q.2 Print numbers from 50 to 40 by decrementing 1======");
+//q.2
+console.warn("Q.2 Print numbers from 50 to 40 by decrementing 1");
 console.log("");
 var result = '';
 for (let index = 50; index >40; index--) {
@@ -18,35 +20,65 @@ for (let index = 50; index >40; index--) {
 console.log(`Numbers 50 to 40:- ${result}`);
 console.log("");
 
-console.warn("===Q.3 First 15 Odd Numbers======");
+console.warn("Q.3 First 15 Odd Numbers");
 console.log("");
 
-// var result = '';
-// var countOdd=0
-// for (let index = 0; index < 100; index++) {
-//     if (index%2!=0) {
-//         countOdd=countOdd+1
-//             if(countEven==15) {
-//               break;
-//             }
-//         result = result +" " + index; 
-//     }
-   
-    
-// }
-console.log(`Numbers 50 to 40:- ${result}`);
-console.log("");
-
-console.log("even no");
+var result ='';
 var countOdd=0
-for (let index = 0; index < 1000; index++) {
-    if (index%2==0) {
-        console.log(index);
-        countodd=countOdd+1;
-        if(countEven==15) {
-            break;
-        }
-    }
+for (let index = 1; index <30; index=index+2) {
+    
+    result = result+" " + index;
+}
+console.log(`First 15 odd numbers are:- ${result}`);
+console.log("");
+
+//que4
+console.warn("Q.4 First 10 Even Numbers");
+console.log("");
+
+var result ='';
+var countOdd=0
+for (let index = 0; index <20; index=index+2) {
+    
+    result = result+" " + index;
+}
+console.log(`First 10 Even numbers are:- ${result}`);
+console.log("");
+
+//que.5
+console.warn("Q.5 Table of 5");
+console.log("");
+
+var result ='';
+var countOdd=0
+for (let index = 5; index <=50; index=index+5) {
+    
+    result = result+" " + index;
+}
+console.log(`Table Of 5:- ${result}`);
+console.log("");
+
+//que6
+console.warn("Q.6 Table of 10");
+console.log("");
+var result = '';
+for (let index = 10; index <=100; index=index+10) {
+    result = result +" " + index;
+    
+}
+console.log(`Table of 10 :- ${result}`);
+console.log("");
+
+//que7
+console.warn("Q.7 Table of 10 in Reverse Order");
+console.log("");
+var result = '';
+for (let index = 100; index >=10; index=index-10) {
+    result = result +" " + index;
+    
+}
+console.log(`Table of 10 in Rev. Order:- ${result}`);
+console.log("");
 
 
 // var result  = '';
