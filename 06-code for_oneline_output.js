54d7c0816f25a5b2bc284223c0d9318143be8248
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-14 00:37:36 -0700

    aa

---
+++ 06-code for_oneline_output.js
@@ -0,0 +1,10 @@
+console.warn("==========");
+
+var result  = '';
+for(var index = 1; index < 11; index +=1) {
+  result = result+" " + index;
+  
+}
+console.log(result)
+
+console.warn("==========");
---
+++ 06_assignment_using_for_loop.js
@@ -0,0 +1,58 @@
+console.warn("===Q.1 Print numbers from 5 to 15 by incrementing 1======");
+console.log("");
+var result = '';
+for (let index = 5; index <=15; index++) {
+    result = result +" " + index;
+    
+}
+console.log(`Numbers 5 to 15:- ${result}`);
+console.log("");
+
+console.warn("===Q.2 Print numbers from 50 to 40 by decrementing 1======");
+console.log("");
+var result = '';
+for (let index = 50; index >40; index--) {
+    result = result +" " + index;
+    
+}
+console.log(`Numbers 50 to 40:- ${result}`);
+console.log("");
+
+console.warn("===Q.3 First 15 Odd Numbers======");
+console.log("");
+
+// var result = '';
+// var countOdd=0
+// for (let index = 0; index < 100; index++) {
+//     if (index%2!=0) {
+//         countOdd=countOdd+1
+//             if(countEven==15) {
+//               break;
+//             }
+//         result = result +" " + index; 
+//     }
+   
+    
+// }
+console.log(`Numbers 50 to 40:- ${result}`);
+console.log("");
+
+console.log("even no");
+var countOdd=0
+for (let index = 0; index < 1000; index++) {
+    if (index%2==0) {
+        console.log(index);
+        countodd=countOdd+1;
+        if(countEven==15) {
+            break;
+        }
+    }
+
+
+// var result  = '';
+// for(var index = 1; index < 11; index +=1) {
+//   result = result+" " + index;
+  
+// }
+// console.log(result)
+
