d62015c13b16ceea53321314f229b01879a8bd38
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-19 20:16:22 -0700

    Create 09_object_basic.js

---
+++ 09_object_basic.js
@@ -0,0 +1,50 @@
+let variable_name='';
+let personDetails={
+    fullName:"Sachin Tendulkar",
+    
+    isMarried:true,
+    age:42,
+    address:{
+        street:"As Club",
+        pin_code: 123456,
+        city:"Pune",
+        state:"MH"
+    },
+    eat:function(){
+        console.log("Iam vegeterian");
+    },
+    walk: function(){
+        console.log("i do walk everyday");
+    }
+    
+}
+console.log(personDetails.eat);
+
+  
+console.log(personDetails.address);
+personDetails.pincode=410210 // to add new property
+personDetails.profession= "cricket"; //to add new property
+
+delete personDetails.age; //to delete property
+
+personDetails.fullName="Sachin Ramesh Tendulkar"// to update the value
+console.log(personDetails);
+
+let personFullName=personDetails.fullName// to access only one property
+console.log(personFullName);
+//or
+console.log(personDetails.fullName);
+
+let marrageStatus=personDetails.isMarried// to access only one property
+console.log(marrageStatus);
+
+//or
+console.log(personDetails["age"]); // to access anathor way
+
+//
+let address={
+    street:"As Club",
+    pin_code: 123456,
+    city:"Pune",
+    state:"MH"
+}
