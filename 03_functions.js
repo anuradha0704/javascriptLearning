a6cab44bdd2925182fa159d819f8ac44080c8876
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-19 18:25:33 -0700

    11

--- 03_functions.js
+++ 03_functions.js
@@ -18,7 +18,7 @@ console.log("//////**************************//////");
 function anuradha(money){
     console.log("please go to market with Rs:-" ,money);
     console.log("buy some vegetables");
-    return "vegetable bag";
+    return "vegetable bag";..
 }
 var veg = anuradha(500);
 console.log(veg);
