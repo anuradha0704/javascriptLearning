74649b2f977a94fd17d471413ccf00fd126f1d7f
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-10-31 01:29:50 -0700

    Create 11_Assign2_on Array.js

---
+++ 11_Assign2_on Array.js
@@ -0,0 +1,72 @@
+console.warn("=================================================Assignent on Array====================================================");
+const array_numbers=[20,31,40,25,23,11,29,9,60,2,11];
+console.log(`Given Array Numbers are:-${array_numbers}`);
+
+console.log("");
+console.warn("Q.1.Total Element Available================================================================================================");
+ let arrayLength=array_numbers.length
+console.log(`Length of Given Array is:-${arrayLength}`);
+
+console.log("");
+console.warn("Q.2. First and last Element ===============================================================================================");
+console.log("First Element of Given Array is:-",array_numbers[0]);
+console.log("Last Element of Given Array is:-",array_numbers[array_numbers.length-1]);
+
+console.log("");
+console.warn("Q.3.Third last Element ===============================================================================================");
+console.log("Third Last Element of Given Array is:-",array_numbers[array_numbers.length-3]);
+
+
+console.log("");
+console.warn("Q.4.All Even Numbers ===============================================================================================");
+var result = '';
+for(num of array_numbers)
+{
+    if (num %2==0) 
+    {
+       result=result+" "+num;
+    }
+        
+}
+console.log("Even Numbers are:-",result)
+
+console.log("");
+console.warn("Q.5.All odd Numbers ===============================================================================================");
+var result = '';
+for(num of array_numbers)
+{
+    if (num %2!=0) 
+    {
+       result=result+" "+num;
+    }
+        
+}
+console.log("Odd Numbers are:-",result)
+
+console.log("");
+console.warn("Q.6. Even position Numbers ===============================================================================================");
+var result = '';
+for (let index = 0; index < array_numbers.length; index++) 
+{
+    if (index%2==0) 
+    {
+        const element=array_numbers[index]
+       result=result+" "+element;
+    }
+        
+}
+console.log("Even position numbers are:-",result)
+
+console.log("");
+console.warn("Q.7. odd position Numbers ===============================================================================================");
+var result = '';
+for (let index = 0; index < array_numbers.length; index++) 
+{
+    if (index%2!=0) 
+    {
+        const element=array_numbers[index]
+       result=result+" "+element;
+    }
+        
+}
+console.log("Odd position numbers are:-",result)
