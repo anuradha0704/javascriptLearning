8ec0bfe0448901fd637f64644243e929ccd03b56
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-11-10 06:16:45 -0800

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,19 @@
+var a= null;
+//console.log(typeOf(car));
+console.log(typeof(a));
+
+var b="3"
+var c="anu"
+console.log(b-c);
+
+var b= function test(){
+    let a="anu"
+  console.log(a);
+}
+test()
+console.log(typeof(b));
+
+var func = function() { 
+    console.log ('Hello World ! ')
+};
+console.log(typeof(func));
