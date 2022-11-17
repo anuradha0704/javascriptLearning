7dff683a67465f6307c5593a5d4e6211dc083a86
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-11-17 09:07:31 -0800

    Update basic.js

--- basic.js
+++ basic.js
@@ -6,14 +6,10 @@ var b="3"
 var c="anu"
 console.log(b-c);
 
-var b= function test(){
+function test(){
     let a="anu"
   console.log(a);
 }
 test()
-console.log(typeof(b));
+console.log(typeof(test()));
 
-var func = function() { 
-    console.log ('Hello World ! ')
-};
-console.log(typeof(func));
