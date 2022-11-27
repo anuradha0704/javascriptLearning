644bdf56f6a7b62fbefcadee6a2223fee56645d7
anuradha0704 <113486754+anuradha0704@users.noreply.github.com>
2022-11-27 02:05:15 -0800

    Update z_2_sring.js

--- z_2_sring.js
+++ z_2_sring.js
@@ -1,25 +1,15 @@
+console.warn("==================reverse the String============================");
 function operationString(words) {
     console.log(`The Given String is:- ${words}`);
     let newString="";
     for (let index = words.length-1; index >=0; index--) {
-        //console.log(index);
-        newString=newString+words[index];
-        //console.log(newString);
+        
+        //newString=newString+words[index];
+        //or
+        newString = newString + words.charAt(index)
     }
     console.log(`Reverse String is:- ${newString}`);
 }
 operationString('TCS the Indian IT Gaint Company')
 
-//program to reverse a string
-
-function reverseString(str) {
-
-    // empty string
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-        
-    }
-    console.log(newString);
-}
-reverseString("hello")
+console.warn("==================count the vovels============================");
